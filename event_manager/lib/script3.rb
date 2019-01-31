ddd67a472ed0926c3aef1ad18bceb745742b5eb3
vincentwang622 <shuyu.wang.0622@gmail.com>
2019-01-31 10:28:46 -0500

    first commit

---
+++ event_manager/lib/script3.rb
@@ -0,0 +1,46 @@
+require 'csv'
+require 'google/apis/civicinfo_v2'
+require 'erb'
+
+def clean_zipcode(zipcode)
+  zipcode.to_s.rjust(5,"0")[0..4]
+end
+
+def legislators_by_zipcode(zip)
+  civic_info = Google::Apis::CivicinfoV2::CivicInfoService.new
+  civic_info.key = 'AIzaSyClRzDqDh5MsXwnCWi0kOiiBivP6JsSyBw'
+
+  begin
+    civic_info.representative_info_by_address(
+      address: zip,
+      levels: 'country',
+      roles: ['legislatorUpperBody', 'legislatorLowerBody']
+    ).officials
+  rescue
+    "You can find your representatives by visiting www.commoncause.org/take-action/find-elected-officials"
+  end
+end
+
+puts "EventManager initialized."
+
+contents = CSV.open 'event_attendees.csv', headers: true, header_converters: :symbol
+
+template_letter = File.read "../form.erb"
+erb_template = ERB.new template_letter
+
+contents.each do |row|
+  name = row[:first_name]
+
+  zipcode = clean_zipcode(row[:zipcode])
+
+  legislators = legislators_by_zipcode(zipcode)
+
+  form_letter = erb_template.result(binding)
+  Dir.mkdir("output") unless Dir.exists? "output"
+
+  filename = "output/thanks.html"
+
+  File.open(filename,'w') do |file|
+    file.puts form_letter
+  end
+end
